009ba77b159baef419dbb4d86e3070030057cd2f
descubrircorrientes <rafael.delvalle@spectec.net>
2022-10-25 21:43:49 -0300

    fix limit json

--- index.js
+++ index.js
@@ -15,8 +15,15 @@ const port = process.env.PORT || 8000;
 
 //middleware
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+
+app.use(express.bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit:50000
+  }));
+
 app.use(historyRoutes);
 app.use(categoriesRoutes);
 app.use(subCategoriesRoutes);
